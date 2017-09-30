c49c851ee6727d6da1790473d0d860364b118bc0
donglin3 <donglin3@ualberta.ca>
2017-09-30 00:01:54 -0600

    editttttbad

--- app/src/main/java/com/example/donglin_countbook/EditCounterActivity.java
+++ app/src/main/java/com/example/donglin_countbook/EditCounterActivity.java
@@ -11,10 +11,102 @@ import android.widget.Toast;
 
 
 public class EditCounterActivity extends AppCompatActivity {
+    EditText nameText;
+    EditText initText;
+    EditText currentText;
+    EditText dateText;
+    EditText commentText;
+    int initvalue;
+    int currentvalue;
+    String name;
+    String comment;
+    String initvalueString;
+    String currentvalueString;
+    Button addButton;
+    Counter CounterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_counter);
+        nameText = (EditText) findViewById(R.id.add_counter_name);
+        initText = (EditText) findViewById(R.id.add_initial_value);
+        currentText = (EditText)findViewById(R.id.add_current_value);
+        dateText = (EditText) findViewById(R.id.add_date);
+        commentText = (EditText)findViewById(R.id.add_comment);
+        dateText.setEnabled(false);
+        addButton= (Button) findViewById(R.id.add);
+
+
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                name = nameText.getText().toString();
+                comment = commentText.getText().toString();
+                initvalueString = initText.getText().toString();
+                currentvalueString = currentText.getText().toString();
+
+
+                if (initvalueString.matches("")){
+                    Toast.makeText(EditCounterActivity.this, "init value is required", Toast.LENGTH_SHORT).show();
+                }
+                else if (name.matches("")){
+                    Toast.makeText(EditCounterActivity.this, "name is required", Toast.LENGTH_SHORT).show();
+                }
+                else if (currentvalueString.matches("")){
+                    Toast.makeText(EditCounterActivity.this, "current value is required", Toast.LENGTH_SHORT).show();
+                }
+                else if (!isStringInt(initvalueString) || !isStringInt(currentvalueString)){
+                    Toast.makeText(EditCounterActivity.this, "init value should be an integer", Toast.LENGTH_SHORT).show();
+                }
+                else if (!isStringInt(currentvalueString)){
+                    Toast.makeText(EditCounterActivity.this, "current value should be an integer", Toast.LENGTH_SHORT).show();
+                }
+                else if (Integer.valueOf(initvalueString)<0||Integer.valueOf(currentvalueString)<0) {
+                    Toast.makeText(EditCounterActivity.this, "init value should be non-negative", Toast.LENGTH_SHORT).show();
+                }
+                else if (Integer.valueOf(currentvalueString)<0) {
+                    Toast.makeText(EditCounterActivity.this, "current value should be non-negative", Toast.LENGTH_SHORT).show();
+                }
+                //Question#1
+//                else if (Integer.valueOf(initString)>Integer.valueOf(currentString)){
+//                    Toast.makeText(edit_page.this, "current value must bigger than or equal to the init value!", Toast.LENGTH_SHORT).show();
+//                }
+                else {
+                    initvalue = Integer.valueOf(initvalueString);
+                    currentvalue = Integer.valueOf(currentvalueString);
+                    //get user's input
+
+                    editCounter.setName(name);
+                    //Question: Do we need to reset the date to current date when the init value is changed?
+                    if (currentvalue!=editCounter.getCurrentValue()) {
+                        editCounter.setDate();
+                    }
+
+                    editCounter.setInitialValue(initvalue);
+                    editCounter.setCurrentValue(currentvalue);
+                    editCounter.setComment(comment);
+                    Intent intent = new Intent();
+                    intent.putExtra("EDITEDCOUNTER", editCounter);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                    //shut down this activity
+                }
+
+
+            }
+        });
     }
+    public boolean isStringInt(String s)
+    {
+        try
+        {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException ex)
+        {
+            return false;
+        }
+    }
+
 }
--- app/src/main/java/com/example/donglin_countbook/MainActivity.java
+++ app/src/main/java/com/example/donglin_countbook/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.donglin_countbook;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
-
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import  com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String FILENAME = "file.sav";
@@ -48,6 +59,48 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<Counter>>() {
+            }.getType();
+            counterList = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            counterList = new ArrayList<Counter>();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+    }
+
+    /**
+     * save all the changes into a file
+     * <br>
+     *  load countersList from the file
+     */
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_PRIVATE);
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+            gson.toJson(counterList, out);
+
+            out.flush();
+
+            fos.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 
 }
